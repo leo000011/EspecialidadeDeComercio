136005641667d5b404d026ac956ceddd27350e22
Leo <leandroano8@gmail.com>
2024-04-16 02:08:28 -0300

    commit

---
+++ js/script2134.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#element", {
+  strings: [
+    "Me chama no Whatsapp 77988526364",
+    "interessado",
+    "Aceitamos todos os tipo de cartão",
+  ],
+  typeSpeed: 50,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
