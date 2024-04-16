b2e99ab28c60c711bc177d38eeac17323b50e365
Leo <leandroano8@gmail.com>
2024-04-16 18:20:05 -0300

    commit

--- js/script2134.js
+++ js/script2134.js
@@ -1,6 +1,7 @@
 var typed = new Typed("#element", {
   strings: [
-    "Ponto de apoio 01 Tanhaçu Bahia",
+    "Ponto de apoio 01 Vitória da Conquista Bahia",
+    "Ponto de apoio 02 Tanhaçu Bahia",
     "Me chama no Whatsapp 77988526364",
     "Aceitamos todos os tipo de cartão",
   ],
